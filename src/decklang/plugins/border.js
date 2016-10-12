8dcdac84beeabc10b594ea3206c790b9271f4849
seiyria <kyle@seiyria.com>
2016-10-12 16:17:58 -0500

    match outline negative with positive

--- src/decklang/plugins/border.js
+++ src/decklang/plugins/border.js
@@ -24,6 +24,7 @@ snippet border
     state.options.card['outline-style'] = style;
     state.options.card['outline-color'] = color;
     state.options.card['outline-width'] = super.combineForUnit(width, state);
+    state.options.card['outline-offset'] = `-${state.options.card['outline-width']}`;
   }
 
 }
